570d3f820338f161797772b8b22fa42d6b92355f
Theron Patrick <theron.patrick@ingramcontent.com>
2016-08-30 22:55:27 -0400

    basic functionality done
    Just need to figure out widths and we're good to go

--- zac/src/App.js
+++ zac/src/App.js
@@ -1,24 +1,85 @@
+
+
 import React, { Component } from 'react';
+import GalleryImage from './GalleryImage'
 import './styles/app.scss'; 
 
 class App extends Component {
+
+  componentWillMount() {
+      this.setState({
+        javascriptEnabled: false,
+        imageIndex: 0,
+        imageArray: []
+      })  
+  }
+
+  _javascriptify = (e) => {
+    let newArray = [<GalleryImage key={0} />]
+    this.setState({
+      imageArray: newArray,
+      javascriptEnabled: true
+    })
+  }
+
+  _nextImage = (e) => {
+
+    let newIndex = this.state.imageIndex + 1
+
+    // Need to make new array since pushing directly to state is considered unsafe (if we push to it)
+    let newArray = this.state.imageArray;
+
+    // If we're at end of array, push new image
+    if (newIndex === this.state.imageArray.length) {
+      newArray.push(<GalleryImage key={newIndex} />);
+    }
+    
+    this.setState({
+      imageArray: newArray,
+      imageIndex: this.state.imageIndex + 1
+    })
+  }
+
+  _pushNewImage () {
+
+  }
+
+  _previousImage = (e) => {
+    if (this.state.imageIndex !== 0) {
+      this.setState({
+        imageIndex: this.state.imageIndex -1
+      })
+    }
+  }
   
   render() {
     let title = "Majestic Stock Photo"
 
+    let currentImage;
+    if (!this.state.javascriptEnabled) {
+      currentImage = <GalleryImage />
+    } else {
+      // Build image gallery and set appropriate style
+      let galleryStyle = {
+        width: 300 * this.state.imageArray.length,
+        marginLeft: 0 - this.state.imageIndex * 300
+      }
+      currentImage = <div className="image-gallery slideshow-transition" style={galleryStyle}>{this.state.imageArray}</div>;
+    }
+
     return (
       <div className="app">
         <div className="app-header">
-          <h1>Pure HTML/CSS Image <button role="button">Click to javascriptify</button></h1>
+          <h1>Pure HTML/CSS Image <button role="button" onClick={this._javascriptify}>Click to javascriptify</button></h1>
         </div>
         <div className="image-container">
             <div className="image-title ease-in">{title} 
               <div className="image-nav-aligner">
-                <button role="button" aria-label="previous">&lt;</button> 
-                <button role="button" aria-label="next">&gt;</button>
+                <button role="button" aria-label="previous" onClick={this._previousImage}>&lt;</button> 
+                <button role="button" aria-label="next" onClick={this._nextImage}>&gt;</button>
               </div>
             </div>
-            <img src="https://unsplash.it/500/300/?random" alt={title} title={title}></img>
+            {currentImage}
             <div className="image-description ease-in">Randomly selected for your viewing pleasure</div>
         </div>
       </div>
---
+++ zac/src/GalleryImage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class GalleryImage extends Component {
+
+  componentWillMount() {
+    // Get width from 300 to 1000 to force a random image (apparently ?random param doesn't work with same dimensions?)
+    let random = Math.floor(Math.random() * (500 - 300) + 300);
+
+    this.setState({
+      loaded: false,
+      src: "https://source.unsplash.com/category/nature/300x" + random
+    })
+  }
+
+  _onLoad = () => {
+    console.log("load image")
+    this.setState({
+      loaded: true
+    })
+  }
+
+  render() {
+    let title = "Majestic Stock Photo"
+    
+
+    let loadingStyle = {
+      opacity: this.state.loaded === true ? 0 : 1
+    }
+
+    return <span className="image-wrapper">
+      <img src={this.state.src} alt={title} title={title} onLoad={this._onLoad}></img>
+      <div className="loading-cover ease-in" style={loadingStyle}>Loading...</div>
+      </span>
+  }
+}
+
+export default GalleryImage;
