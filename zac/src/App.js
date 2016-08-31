d37f5a578c04050cc5682c27d1114e5025b20011
Theron Patrick <theron.patrick@ingramcontent.com>
2016-08-30 20:57:02 -0400

    Plain html/css image

--- zac/src/App.js
+++ zac/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './styles/app.scss'; 
 
 class App extends Component {
+  
   render() {
+    let title = "Majestic Stock Photo"
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="app-header">
+          <h1>Pure HTML/CSS Image <button role="button">Click to javascriptify</button></h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="image-container">
+            <div className="image-title ease-in">{title} <button role="button" aria-label="previous">&lt;</button> <button role="button" aria-label="next">&gt;</button></div>
+            <img src="https://unsplash.it/300/300/?random" alt={title} title={title}></img>
+            <div className="image-description ease-in">Randomly selected for your viewing pleasure</div>
+        </div>
       </div>
     );
   }
