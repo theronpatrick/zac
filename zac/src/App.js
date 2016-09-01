0416b90df306d4de3bd3a07654093f04b6866dd8
Theron Patrick <theron.patrick@ingramcontent.com>
2016-08-31 22:19:55 -0400

    clean up some comments

--- zac/src/App.js
+++ zac/src/App.js
@@ -1,5 +1,3 @@
-
-
 import React, { Component } from 'react';
 import GalleryImage from './GalleryImage'
 import './styles/app.scss'; 
--- zac/src/GalleryImage.js
+++ zac/src/GalleryImage.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class GalleryImage extends Component {
 
   componentWillMount() {
-    // Get width from 300 to 1000 to force a random image (apparently ?random param doesn't work with same dimensions?)
+    // Get width from 300 to 1000 to force a random image 
+    // (note that this doesn't always work, but it seems better than using the same dimensions)
     let random = Math.floor(Math.random() * (500 - 300) + 300);
 
     this.setState({
@@ -21,7 +22,6 @@ class GalleryImage extends Component {
   render() {
     let title = "Majestic Stock Photo"
     
-
     let loadingStyle = {
       opacity: this.state.loaded === true ? 0 : 1
     }
